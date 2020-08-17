216c9d3d6ff6a19a25f2baee69393dc87321b924
miya1186 <66915986+miya1186@users.noreply.github.com>
2020-08-18 08:27:35 +0900

    first commit

---
+++ tableView/tableView/AddViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddViewController.swift
+//  tableView
+//
+//  Created by miyazawaryohei on 2020/08/18.
+//  Copyright © 2020 miya. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var addTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
---
+++ tableView/tableView/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by miyazawaryohei on 2020/08/17.
+//  Copyright © 2020 miya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet private var tableView: UITableView!
+    private var fruitItems :[Dictionary<String, Any>] = []
+    
+    private let KeyName = "Name"
+    private let KeyCheck = "Check"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        fruitItems = [[KeyName:"りんご",KeyCheck:false,],
+                      [KeyName:"みかん",KeyCheck:true,],
+                      [KeyName:"バナナ",KeyCheck:false,],
+                      [KeyName:"パイナップル",KeyCheck:true,]
+        ]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.fruitItems.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        
+        let fruitItem = self.fruitItems[indexPath.row]
+        cell.cellImage.image = nil
+        
+        if fruitItem[KeyCheck] as? Bool == true {
+            cell.cellImage.image = UIImage(named: "checkmark")
+        }
+        
+        cell.label.text = (fruitItem[KeyName] as? String) ?? ""
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //課題15 Bool値が true → false  false　→ true
+        if fruitItems[indexPath.row][KeyCheck] as! Bool == true{
+            fruitItems[indexPath.row].updateValue(false,forKey: KeyCheck)
+        }else{
+            fruitItems[indexPath.row].updateValue(true,forKey: KeyCheck)
+        }
+        tableView.reloadData()
+    }
+    
+    
+    @IBAction func exitCancell(segue:UIStoryboardSegue){
+    }
+    
+    
+    @IBAction func exitSave(segue:UIStoryboardSegue){
+        let AddVC = segue.source as! AddViewController
+        let addText = AddVC.addTextField.text
+        
+        //空の配列が入ることを防ぐ
+        if addText != ""{
+            fruitItems.append([KeyName:addText!,KeyCheck:false])
+        }
+        tableView.reloadData()
+    }
+    
+}
+
